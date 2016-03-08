6c843ab4f5ce10fa84ff5a785c3a6a75092c32d7
AladdinKamal <aladdin.kamal7@gmail.com>
2016-03-08 22:41:36 +0200

    Add new features

---
+++ Renamp/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Renamp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Project name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "New project name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(193, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(223, 24);
+            this.textBox1.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.textBox1, "Your old project should be located in \'C:\\Users\\xxx\\Documents\\Visual Studio 2015\\" +
+        "Projects\'.");
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(193, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(223, 24);
+            this.textBox2.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.textBox2, "The new project\'s name can only contain letters, numbers, - and _.");
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(235, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 37);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Rename";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(461, 28);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(45, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(29, 142);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(163, 21);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Not default directory?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(193, 139);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(223, 24);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Dialog;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(461, 194);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Renamp";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Renamp/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Renamp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(@"-The default projects' directory is\n   'C:\Users\XXX\Documents\Visual Studio 2015\Projects\ '.\n-The new project's name can only contain letters, numbers, - and _. ", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetterOrDigit(e.KeyChar) && e.KeyChar != '_' && e.KeyChar != '-' && e.KeyChar != (char)8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim() == "" || textBox2.Text.Trim() == "" || textBox3.Text.Trim() == "" && checkBox1.Checked == true)
+            {
+                MessageBox.Show("Please fill all the fields.");
+                return;
+            }
+            else if (textBox1.Text == textBox2.Text)
+            {
+                MessageBox.Show("Please change new project name.");
+            }
+
+            String newName = textBox2.Text;
+            String oldName1 = textBox1.Text;
+            String oldName2 = "";
+            if (oldName1.Contains(' '))
+            {
+                oldName2 = oldName1;
+                oldName2 = oldName2.Replace(' ', '_');
+            }
+
+            String oldPath;
+            String newPath;
+            String Backup;
+
+            if (textBox3.Text.Trim() != "")
+            {
+                oldPath = textBox3.Text + @"\" + oldName1 + @"\";
+                newPath = textBox3.Text + @"\" + newName + @"\";
+                Backup = textBox3.Text + @"\Renamp Backup\" + oldName1 + @"\";
+                String oldPathx = oldPath + oldName1 + @"\";
+                if (!Directory.Exists(oldPathx))
+                {
+                    MessageBox.Show("Incorrect project name or directory, please try again.");
+                    return;
+                }
+            }
+            else
+            {
+                oldPath = @"C:\Users\" + Environment.UserName + @"\Documents\Visual Studio 2015\Projects\" + oldName1 + @"\";
+                newPath = @"C:\Users\" + Environment.UserName + @"\Documents\Visual Studio 2015\Projects\" + newName + @"\";
+                Backup = @"C:\Users\" + Environment.UserName + @"\Documents\Visual Studio 2015\Projects\Renamp Backup\" + oldName1 + @"\";
+                if (!Directory.Exists(oldPath))
+                {
+                    MessageBox.Show("Incorrect project name, please try again.");
+                    return;
+                }
+            }
+
+            foreach (string dirPath in Directory.GetDirectories(oldPath, "*",
+                SearchOption.AllDirectories))
+                Directory.CreateDirectory(dirPath.Replace(oldPath, Backup));
+
+            foreach (string newPath1 in Directory.GetFiles(oldPath, "*.*",
+                SearchOption.AllDirectories))
+                File.Copy(newPath1, newPath1.Replace(oldPath, Backup), true);
+
+            Directory.Move(oldPath, newPath);
+
+            System.IO.File.Move(newPath + oldName1 + ".sln", newPath + newName + ".sln");
+
+            fileEditor(newPath + newName + ".sln", oldName1, newName, oldName2);
+
+            oldPath = newPath + oldName1 + @"\";
+            newPath += newName + @"\";
+            Directory.Move(oldPath, newPath);
+
+            if (Directory.Exists(newPath + @"bin\"))
+                DeleteDirectory(newPath + @"bin\");
+            if (Directory.Exists(newPath + @"obj\"))
+                DeleteDirectory(newPath + @"obj\");
+
+            System.IO.File.Move(newPath + oldName1 + ".csproj", newPath + newName + ".csproj");
+
+            fileEditor(newPath + newName + ".csproj", oldName1, newName, oldName2);
+            fileEditor(newPath + "Program.cs", oldName1, newName, oldName2);
+            fileEditor(newPath + "Form1.cs", oldName1, newName, oldName2);
+            fileEditor(newPath + "Form1.Designer.cs", oldName1, newName, oldName2);
+
+            if (File.Exists(newPath + "Form2.cs"))
+            {
+                fileEditor(newPath + "Form2.cs", oldName1, newName, oldName2);
+                fileEditor(newPath + "Form2.Designer.cs", oldName1, newName, oldName2);
+            }
+            if (File.Exists(newPath + "Form3.cs"))
+            {
+                fileEditor(newPath + "Form3.cs", oldName1, newName, oldName2);
+                fileEditor(newPath + "Form3.Designer.cs", oldName1, newName, oldName2);
+            }
+            if (File.Exists(newPath + "Form4.cs"))
+            {
+                fileEditor(newPath + "Form4.cs", oldName1, newName, oldName2);
+                fileEditor(newPath + "Form4.Designer.cs", oldName1, newName, oldName2);
+            }
+            if (File.Exists(newPath + "Form5.cs"))
+            {
+                fileEditor(newPath + "Form5.cs", oldName1, newName, oldName2);
+                fileEditor(newPath + "Form5.Designer.cs", oldName1, newName, oldName2);
+            }
+            if (File.Exists(newPath + "Form6.cs"))
+            {
+                fileEditor(newPath + "Form6.cs", oldName1, newName, oldName2);
+                fileEditor(newPath + "Form6.Designer.cs", oldName1, newName, oldName2);
+            }
+            if (File.Exists(newPath + "Form7.cs"))
+            {
+                fileEditor(newPath + "Form7.cs", oldName1, newName, oldName2);
+                fileEditor(newPath + "Form7.Designer.cs", oldName1, newName, oldName2);
+            }
+            if (File.Exists(newPath + "Form8.cs"))
+            {
+                fileEditor(newPath + "Form8.cs", oldName1, newName, oldName2);
+                fileEditor(newPath + "Form8.Designer.cs", oldName1, newName, oldName2);
+            }
+            if (File.Exists(newPath + "Form9.cs"))
+            {
+                fileEditor(newPath + "Form9.cs", oldName1, newName, oldName2);
+                fileEditor(newPath + "Form9.Designer.cs", oldName1, newName, oldName2);
+            }
+            if (File.Exists(newPath + "Form10.cs"))
+            {
+                fileEditor(newPath + "Form10.cs", oldName1, newName, oldName2);
+                fileEditor(newPath + "Form10.Designer.cs", oldName1, newName, oldName2);
+            }
+
+            newPath = newPath + @"Properties\";
+
+            fileEditor(newPath + "AssemblyInfo.cs", oldName1, newName, oldName2);
+            fileEditor(newPath + "Resources.Designer.cs", oldName1, newName, oldName2);
+            fileEditor(newPath + "Settings.Designer.cs", oldName1, newName, oldName2);
+
+            DialogResult result = MessageBox.Show("Project name has been successfully changed.\nDo you want to change another project name ?", "SUCCESS!!", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (result == DialogResult.No)
+            {
+                Application.Exit();
+            }
+            else if (result == DialogResult.Yes)
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                this.ClientSize = new System.Drawing.Size(395, 180);
+                button1.Location = new System.Drawing.Point(200, 140);
+                textBox3.Visible = true;
+            }
+            else
+            {
+                this.ClientSize = new System.Drawing.Size(395, 150);
+                button1.Location = new System.Drawing.Point(200, 110);
+                textBox3.Visible = false;
+                textBox3.Text = "";
+
+            }
+        }
+        public static void DeleteDirectory(string target_dir)
+        {
+            string[] files = Directory.GetFiles(target_dir);
+            string[] dirs = Directory.GetDirectories(target_dir);
+
+            foreach (string file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+
+            foreach (string dir in dirs)
+            {
+                DeleteDirectory(dir);
+            }
+
+            Directory.Delete(target_dir, false);
+        }
+
+        public static void fileEditor(String path, String oldN1, String newN, String oldN2)
+        {
+            StreamReader reader = new StreamReader(path);
+            string input = reader.ReadToEnd();
+            reader.Close();
+            input = input.Replace(oldN1, newN);
+            if (oldN2 != "")
+                input = input.Replace(oldN2, newN);
+            StreamWriter writer = new StreamWriter(path);
+            writer.Write(input);
+            writer.Close();
+        }
+    }
+}
